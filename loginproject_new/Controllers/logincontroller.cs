e231522f921b29c04d7b988b3c566dbf33cfc816
Serhat Demir <64663453+wzarok@users.noreply.github.com>
2021-12-23 16:21:01 +0300

    ASP.Net Login Page
    ASP.Net Login Page

---
+++ loginproject_new/Controllers/logincontroller.cs
@@ -0,0 +1,30 @@
+﻿using loginproject_new.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace loginproject_new.Controllers
+{
+    public class logincontroller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessLogin(usermodel usermodel)
+        {
+            if (usermodel.username == "serhat" && usermodel.password == "demir")
+            {
+                return View("Login Success", usermodel);
+            }
+            else
+            {
+                return View("Login Failure", usermodel);
+            }
+            
+        }
+    }
+}
---
+++ loginproject_new/Models/usermodel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace loginproject_new.Models
+{
+    public class usermodel
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
